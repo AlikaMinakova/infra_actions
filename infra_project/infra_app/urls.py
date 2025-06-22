7fd08d8e648d96f06c239df386a91e93b0d99009
AlikaMinakova <minackowa.ali@yandex.ru>
2025-06-22 10:17:16 +0300

    correct urls.py and tests.py for workflow

--- infra_project/infra_app/urls.py
+++ infra_project/infra_app/urls.py
@@ -9,3 +9,8 @@ urlpatterns = [
     path('second/', views.second_page, name='second_page'),
 
 ]
+
+# flake8 . - проверить что workflow на github
+# actions пройдёт корректно
+# isort infra_project/urls.py - если не можешь исправить
+# файл вручную напиши это для неправильного файла
